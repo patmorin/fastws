3e5b5c71f9ed70378e912a426b82a4c216d402b3
Pat Morin <morin@scs.carleton.ca>
2014-06-20 07:56:02 -0400

    Starting some experiments in earnest

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -4,7 +4,11 @@
 #include <ctime>
 using namespace std;
 
+#include "SkiplistSSet.h"
+#include "Treap.h"
+#include "SplayTree.h"
 #include "wsskiplist.h"
+#include "topskiplist.h"
 
 
 int int_cmp(const int &a, const int &b) {
@@ -18,7 +22,7 @@ void big_test(int n, double eps) {
 		data[i] = i + 1;
 	{
 		cout << "Creating WSSkiplist" << endl;
-		fastws::WSSkiplist<int> sl(data, n, int_cmp, eps);
+		fastws::TopSkiplist<int> sl(data, n, int_cmp, eps);
 		delete[] data;
 		cout << "Doing " << 4*n << " searches" << endl;
 		for (int i = 0; i < 4*n; i++) {
@@ -31,7 +35,7 @@ void big_test(int n, double eps) {
 }
 
 template<class Dict>
-void random_searches(Dict &d, char *name) {
+void random_searches(Dict &d, const char *name) {
 	clock_t start = clock();
 	int n = d.size();
 	for (int i = 0; i < 3*n; i++) {
@@ -63,7 +67,7 @@ void fractional_searches(Dict &d, char *name, double percentage) {
 
 
 int main(int argc, char **argv) {
-	int n = 1000;
+	int n = 10000;
 	cout << argc << endl;
 	if (argc == 2)
 		n = atoi(argv[1]);
@@ -72,17 +76,38 @@ int main(int argc, char **argv) {
 	for (int i = 0; i < n; i++)
 		data[i] = i + 1;
 
+	for (double percentage = 1; percentage > 0; percentage -= .01) {
+		ods::SplayTree1<int> st;
+		for (int i = 0; i < n; i++)
+			st.add(data[i]);
+		fractional_searches<ods::SplayTree1<int> >(st, "SplayTree", percentage);
+	}
+
+	for (double percentage = 1; percentage > 0; percentage -= .01) {
+		ods::Treap1<int> sl;
+		for (int i = 0; i < n; i++)
+			sl.add(data[i]);
+		fractional_searches<ods::Treap1<int> >(sl, "Treap", percentage);
+	}
+
+	for (double percentage = 1; percentage > 0; percentage -= .01) {
+		ods::SkiplistSSet<int> sl;
+		for (int i = 0; i < n; i++)
+			sl.add(data[i]);
+		fractional_searches<ods::SkiplistSSet<int> >(sl, "Skiplist", percentage);
+	}
+
 	double epsilon = .45;
 	for (double percentage = 1; percentage > 0; percentage -= .01) {
-		fastws::WSSkiplist<int> sl(data, n, int_cmp, epsilon);
+		fastws::TopSkiplist<int> sl(data, n, int_cmp, epsilon);
 		char name[100];
 		sprintf(name, "WSSkiplist(%lf)", epsilon);
-		fractional_searches<fastws::WSSkiplist<int> >(sl, name, percentage);
+		fractional_searches<fastws::TopSkiplist<int> >(sl, name, percentage);
 	}
 
 
 /*  Using the following code, we determined that the optimal for epsilon
- *  is about .45
+ *  is about .45; but the running-times are _very_ sensitive to epsilon
 	for (double epsilon = .6; epsilon >= .1; epsilon -= .01) {
 		fastws::WSSkiplist<int> sl(data, n, int_cmp, epsilon);
 		char name[100];
@@ -118,7 +143,7 @@ int main(int argc, char **argv) {
 	cout << "Done!" << endl;
 */
 
-	big_test(n, .44);
+	// big_test(n, .44);
 	return 0;
 }
 
---
+++ cpp/topskiplist.h
@@ -0,0 +1,267 @@
+/**
+ * (c) 2014 Pat Morin, Released under a Creative Commons Attribution (CC-BY)
+ *     license.
+ *
+ * wsskiplist.h : An implementation of the working-set skiplist structure
+ *
+ * This particular implementation is a space hog.  Every element in the
+ * structure has its own array of length k=Theta(log n)$ that is used to
+ * store its previous and next pointers.  This avoids the allocating and
+ * freeing of nodes when nodes are promoted or levels are rebuilt.
+ */
+#ifndef FASTWS_TOPSKIPLIST_H_
+#define FASTWS_TOPSKIPLIST_H_
+
+#include <cmath>
+#include <cstring>
+#include <cstdlib>
+#include <climits>
+#include <cassert>
+
+#include <iostream>
+using namespace std;
+
+namespace fastws {
+
+/**
+ * A dictionary with the working-set property.
+ */
+template<class T>
+class TopSkiplist {
+protected:
+	struct NP;
+
+	struct Node {
+		int w; // the working-set number (not always correct)
+		T x;   // data
+
+		// these maintain a global queue ordered by working-set number
+		Node *qnext;
+		Node *qprev;
+
+		Node *next[]; // a stack of next pointers
+	};
+
+	int k;    // there are k+1 lists numbered 0,...,k
+	int *n;   // n[i] is the size of the i'th list
+	Node *sentinel; // sentinel-next[i] is the first element of list i
+
+	// parameters used to determine lists sizes
+	double eps;
+	int n0max;
+	int *a;
+	int *b;
+
+	// FIXME: for profiling information
+	int *rebuild_freqs;
+
+	void init(T *data, int n);
+	void rebuild(int i);
+
+	void sanity();
+
+	Node *newNode();
+	void deleteNode(Node *u);
+
+	// FIXME: integer only
+	int (*cmp)(const T &a, const T &b);
+
+public:
+	TopSkiplist(T *data, int n0, int (*cmp0)(const T&, const T&),
+			double eps0);
+	virtual ~TopSkiplist();
+	T find(T x);
+	int size() {
+		return n[k];
+	}
+
+	void printOn(std::ostream &out);
+};
+
+template<class T>
+TopSkiplist<T>::TopSkiplist(T *data, int n0, int (*cmp0)(const T&, const T&),
+	double eps0) {
+	eps = eps0;
+	cmp = cmp0;
+	init(data, n0);
+}
+
+template<class T>
+void TopSkiplist<T>::init(T *data, int n0) {
+
+	// Compute critical values depending on epsilon
+	n0max = ceil(2. / eps);
+	cout << "n0max = " << n0max << endl;
+	double base_a = 2.0-eps;
+	double base_b = 2.0-eps/2;
+	k = ceil(log(n0) / log(base_a));
+	a = new int[k + 1];
+	b = new int[k + 1];
+	int offset = ceil(log(n0max)/log(base_b))-1;
+	k -= offset;
+	for (int i = 0; i <= k; i++) {
+		a[i] = pow(base_a, i+offset);
+		b[i] = pow(base_b, i+offset);
+		cout << "a[" << i << "]=" << a[i] << ", b[" << i << "]=" << b[i]
+				<< endl;
+	}
+
+	rebuild_freqs = new int[k + 1]();
+
+	n = new int[k + 1]();
+
+	n[k] = n0;
+	sentinel = newNode();
+	sentinel->x = -1; // FIXME: non-negative integer only
+	sentinel->qnext = sentinel->qprev = sentinel;
+	Node *prev = sentinel;
+	for (int i = 0; i < n0; i++) {
+		Node *u = newNode();
+		u->x = data[i];
+		prev->next[k] = u;
+		u->qprev = prev;
+		prev->qnext = u;
+		prev = u;
+	}
+	prev->qnext = sentinel; // the queue is a circular list
+	rebuild(k);
+}
+
+template<class T>
+typename TopSkiplist<T>::Node* TopSkiplist<T>::newNode() {
+	Node *u = (Node *) malloc(sizeof(Node) + (k + 1) * sizeof(Node*));
+	u->qnext = u->qprev = NULL;
+	u->w = INT_MAX;
+	memset(u->next, '\0', (k + 1) * sizeof(Node*));
+	return u;
+}
+
+template<class T>
+void TopSkiplist<T>::deleteNode(Node *u) {
+	free(u);
+}
+
+template<class T>
+void TopSkiplist<T>::rebuild(int i) {
+
+	rebuild_freqs[i]++;
+
+	// compute working-set numbers of relevant nodes
+	Node *u = sentinel->qnext;
+	int wmax = a[i - 1];
+	for (int t = 0; t <= wmax; t++) {
+		u->w = t + 1;
+		u = u->qnext;
+	}
+
+	for (int j = i - 1; j >= 0; j--) {
+		// populate L_j using L_{j+1}
+		n[j] = 0;
+		u = sentinel->next[j + 1];
+		Node *prev = sentinel;
+		int w = a[j];
+		bool skipped = false;
+		while (u != NULL) {
+			if (skipped || u->w <= w) {
+				prev->next[j] = u;
+				prev = u;
+				n[j]++;
+				skipped = false;
+			} else {
+				skipped = true;
+			}
+			u = u->next[j + 1];
+		}
+		prev->next[j] = NULL;
+	}
+
+	// reset all working-set numbers
+	u = sentinel->qnext;
+	for (int t = 0; t < wmax; t++) {
+		u->w = INT_MAX;
+		u = u->qnext;
+	}
+
+}
+
+template<class T>
+T TopSkiplist<T>::find(T x) {
+	Node *u = sentinel;
+	int i = 0;
+	while (u->next[i] != NULL && u->next[i]->x < x)
+		u = u->next[i];
+	for (i = 1; i <= k; i++) {
+		if (u->next[i] != NULL && u->next[i]->x < x)
+			u = u->next[i];
+	}
+
+	// Search is done: we're going to return w->x
+	i = i > k ? k : i;
+	Node *w = u->next[i];
+	if (w == NULL)
+		return (T) NULL;  // FIXME: not portable
+	return w->x;
+}
+
+template<class T>
+TopSkiplist<T>::~TopSkiplist() {
+	Node *prev = sentinel;
+	while (prev != NULL) {
+		Node *u = prev->next[k];
+		prev->next[k] = NULL;
+		deleteNode(prev);
+		prev = u;
+	}
+}
+
+template<class T>
+void TopSkiplist<T>::sanity() {
+	assert(n[0] <= n0max);
+	for (int i = 0; i <= k; i++) {
+		Node *u = sentinel;
+		for (int j = 0; j < n[i]; j++) {
+			assert(u == sentinel || u->x < u->next->x);
+			assert(u->w == INT_MAX);
+			u = u->next[i];
+		}
+		assert(u->next[i] == NULL);
+	}
+}
+
+template<class T>
+void TopSkiplist<T>::printOn(std::ostream &out) {
+	const int max_print = 50;
+	cout << "WSSkiplist: n = " << n[k] << ", k = " << k << endl;
+	for (int i = 0; i <= k; i++) {
+		cout << "L(" << i << "): ";
+		if (n[k] <= max_print) {
+			Node *u = sentinel->next[i];
+			for (int j = 0; j < n[i]; j++) {
+				cout << u->x << ",";
+				u = u->next[i];
+			}
+			assert(u == NULL);
+		}
+		cout << " n(" << i << ") = " << n[i]
+		     << " (rebuilt " << rebuild_freqs[i] << " times)" << endl;
+	}
+	if (n[k] <= max_print) {
+		cout << "q = ";
+		Node *u = sentinel->qnext;
+		while (u != sentinel) {
+			cout << u->x << ",";
+			u = u->qnext;
+		}
+		cout << endl;
+	}
+}
+
+template<class T>
+ostream& operator<<(ostream &out, TopSkiplist<T> &sl) {
+	sl.printOn(out);
+	return out;
+}
+
+} // fastws namespace
+
+#endif // FASTWS_TOPSKIPLIST_H_
